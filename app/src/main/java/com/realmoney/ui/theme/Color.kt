04f43fd6290e72cd2256bb1620426347cb11137c
Eben276 <nebenezer276@gmail.com>
2025-04-05 15:02:05 +0000

    Add app/src/main/java/com/realmoney/ui/theme/Color.kt 1743865318964

---
+++ app/src/main/java/com/realmoney/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.realmoney.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MoneyGreen = Color(0xFF4CAF50)
+val AlertRed = Color(0xFFF44336)
+val PrimaryColor = Color(0xFF6200EE)
+val SecondaryColor = Color(0xFF03DAC6)
+val ErrorColor = Color(0xFFB00020)
+val BackgroundColor = Color(0xFFFFFFFF)
+val SurfaceColor = Color(0xFFFFFFFF)
+val OnPrimaryColor = Color(0xFFFFFFFF)
+val OnSecondaryColor = Color(0xFF000000)
+val OnBackgroundColor = Color(0xFF000000)
+val OnSurfaceColor = Color(0xFF000000)
+val OnErrorColor = Color(0xFFFFFFFF)
